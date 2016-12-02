b8319f8dd442dde8a05dccbf1d0ef35759d9b5e1
captainjspace <joshualandman@yahoo.com>
2016-12-02 08:44:59 -0800

    added shell scripts to root to run and test api

---
+++ run_api_check.sh
@@ -0,0 +1,7 @@
+curl http://localhost:8080/test
+curl http://localhost:8080/test?name=BIRD
+curl http://localhost:8081
+curl http://localhost:8081/metrics
+curl http://localhost:8081/ping
+curl http://localhost:8081/threads
+curl http://localhost:8081/healthcheck
---
+++ run_dropwizard.sh
@@ -0,0 +1,1 @@
+java -jar target/DropTest-0.0.1-SNAPSHOT.jar server basic.yml
